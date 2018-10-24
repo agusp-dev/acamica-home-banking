0ccba23bef17d877b70ffe68c14d83ff5b7b574d
agustin.perez <email>
2018-10-24 01:37:42 -0300

    Se agrega funcionalidad de Pago de Servicios. Se toma en cuenta boton cancelar de prompts.

--- js/homebanking.js
+++ js/homebanking.js
@@ -3,6 +3,14 @@ var nombreUsuario = "Erika Moreno";
 var saldoCuenta = 20000;
 var limiteExtraccion = 3000;
 
+const COD_AGUA = 1;
+const COD_LUZ = 2;
+const COD_INTERNET = 3;
+const COD_TELEFONO = 4;
+
+const SERVICIOS_NOMBRES = new Map([[COD_AGUA, "Agua"], [COD_LUZ, "Luz"], [COD_INTERNET, "Internet"], [COD_TELEFONO, "Teléfono"]]); 
+const SERVICIOS_MONTOS = new Map([[COD_AGUA, 350], [COD_LUZ, 425], [COD_INTERNET, 210], [COD_TELEFONO, 570]]);
+
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
     cargarNombreEnPantalla();
@@ -13,7 +21,13 @@ window.onload = function() {
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-    var limite = parseInt(prompt("Por favor, ingrese su nuevo límite de extracción:"));
+
+    var alertResultado = prompt("Por favor, ingrese su nuevo límite de extracción:", limiteExtraccion);
+    if (alertResultado == null) {
+        return;
+    }
+
+    var limite = parseInt(alertResultado);
     if (isNaN(limite)) {
         mostrarMensajeErrorMontoIngresado();
         return;
@@ -25,7 +39,14 @@ function cambiarLimiteDeExtraccion() {
 }
 
 function extraerDinero() {
-    var cantExtraccion = parseInt(prompt("Por favor, ingrese la cantidad de dinero a extraer:"));
+
+    var alertResultado = prompt("Por favor, ingrese la cantidad de dinero a extraer:");
+    if (alertResultado == null) {
+        return;
+    }
+
+    var cantExtraccion = parseInt(alertResultado);
+    console.log(cantExtraccion);
     if (isNaN(cantExtraccion)) {
         mostrarMensajeErrorMontoIngresado();
         return;
@@ -41,6 +62,11 @@ function extraerDinero() {
         return;
     }
 
+    if (!entregaEnBilletesDeCien(cantExtraccion)) {
+        mostrarMensajeMontoNoPuedeEntregarseEnBilletesDeCien();
+        return;
+    }
+
     var saldoAnterior = saldoCuenta;
     restarDineroAlSaldo(cantExtraccion);
 
@@ -49,7 +75,12 @@ function extraerDinero() {
 }
 
 function depositarDinero() {
-    var cantDeposito = parseInt(prompt("Por favor, ingrese la cantidad de dinero a depositar:"));
+    var alertResultado = prompt("Por favor, ingrese la cantidad de dinero a depositar:");
+    if (alertResultado == null) {
+        return;
+    }
+
+    var cantDeposito = parseInt(alertResultado);
     if (isNaN(cantDeposito)) {
         mostrarMensajeErrorMontoIngresado();
         return;
@@ -61,7 +92,37 @@ function depositarDinero() {
 }
 
 function pagarServicio() {
+    var msg = "ATENCIÓN"
+        .concat("\n\nPor favor, ingrese el número que corresponda con el servicio que desea pagar: \n")
+        .concat(obtenerListaDeServiciosString());
 
+    var alertResultado = prompt(msg);
+    if (alertResultado == null) {
+        return;
+    }
+
+    var codigoServicio = parseInt(alertResultado);
+    if (isNaN(codigoServicio)) {
+        mostrarMensajeErrorCodigoServicio();
+        return;
+    }
+
+    var nombreServicio = obtenerNombreServicio(codigoServicio);
+    var precioServicio = obtenerPrecioServicio(codigoServicio);
+    if (nombreServicio == undefined || precioServicio == undefined) {
+        mostrarMensajeServicioSeleccionadoIncorrecto(codigoServicio);
+        return;
+    }
+
+    if (saldoSuperado(precioServicio)) {
+        mostrarMensajeErrorPagoServicioSaldoSuperado(nombreServicio);
+        return;
+    }
+
+    var saldoAnterior = saldoCuenta;
+    restarDineroAlSaldo(precioServicio);
+    mostrarMensajePagoServicioRealizado(nombreServicio, precioServicio, saldoAnterior);
+    actualizarSaldoEnPantalla();
 }
 
 function transferirDinero() {
@@ -105,6 +166,33 @@ function sumarDineroAlSaldo(dinero) {
 function restarDineroAlSaldo(dinero) {
     console.log("Se resta $" + dinero + " al saldo de la cuenta.");
     saldoCuenta -= dinero;
+}
+
+/**
+ * Recorre lista de Servicios y devuelve string con un servicio debajo de otro.
+ */
+function obtenerListaDeServiciosString() {
+    var listaString = "";
+    for (var servicio of SERVICIOS_NOMBRES.entries()) {
+        listaString += servicio[0] + " - " + servicio[1] + "\n";
+    }
+    return listaString;
+}
+
+/**
+ * Obtiene nombre de servicio a partir del codigo.
+ * @param {Number} codigo 
+ */
+function obtenerNombreServicio(codigo) {
+    return SERVICIOS_NOMBRES.get(codigo);
+}
+
+/**
+ * Obtiene precio de servicio a partir del codigo.
+ * @param {Number} codigo 
+ */
+function obtenerPrecioServicio(codigo) {
+    return SERVICIOS_MONTOS.get(codigo);
 }
 
 
@@ -174,6 +262,53 @@ function mostrarMensajeLimiteExtraccionSuperado() {
     alert(msg);
 }
 
+/**
+ * Muestra un alert indicando que el monto ingresado no puede entregarse en billetes de $100.
+ */
+function mostrarMensajeMontoNoPuedeEntregarseEnBilletesDeCien() {
+    var msg = "ATENCIÓN"
+        .concat("\n\nEl monto ingresado no puede extraerse en billetes de $100.");
+    alert(msg);
+}
+
+/**
+ * Muestra un alert indicando que el codigo de servicio ingresado, es incorrecto.
+ * @param {Number} codigo 
+ */
+function mostrarMensajeServicioSeleccionadoIncorrecto(codigo) {
+    var msg = "ERROR"
+        .concat("\n\nEl código ingresado (" + codigo + ") no pertenece a ningún servicio.")
+        .concat("\nLa operación no pudo ser ejecutada.");
+    alert(msg);
+}
+
+/**
+ * Muestra un alert indicando que el monto del servicio supera el saldo actual de la cuenta.
+ * @param {String} nombreServicio 
+ */
+function mostrarMensajeErrorPagoServicioSaldoSuperado(nombreServicio) {
+    var msg = "ERROR"
+        .concat("\n\nEl monto del servicio (" + nombreServicio + ") supera el saldo actual de su cuenta.")
+        .concat("\nLa operación no pudo ser ejecutada.");
+    alert(msg);
+}
+
+function mostrarMensajePagoServicioRealizado(nombreServicio, montoPagado, saldoAnterior) {
+    var msg = "ÉXITO"
+        .concat("\n\nHas pagado el servicio: " + nombreServicio)
+        .concat("\nSaldo anterior: $" + saldoAnterior)
+        .concat("\nDinero descontado: $" + montoPagado)
+        .concat("\nSaldo actual: $" + saldoCuenta);
+    alert(msg);
+}
+
+
+
+
+
+
+
+
 
 //Validaciones
 /**
@@ -190,6 +325,17 @@ function saldoSuperado(monto) {
  */
 function limiteExtraccionSuperado(monto) {
     return (monto > limiteExtraccion);
+}
+
+/**
+ * Verifica si el monto ingresado puede ser entregado en billetes de $100.
+ * @param {Number} monto 
+ */
+function entregaEnBilletesDeCien(monto) {
+    if ((monto % 100) === 0) {
+        return true;
+    }
+    return false;
 }
 
 
